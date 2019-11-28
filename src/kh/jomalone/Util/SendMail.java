129dc3e9047ded13a01c52c94dc5a5abd46e3a93
gh <gh@naver.com>
2019-11-28 10:23:07 +0900

    utf-8 added

--- src/kh/jomalone/Util/SendMail.java
+++ src/kh/jomalone/Util/SendMail.java
@@ -13,11 +13,11 @@ import javax.mail.internet.MimeMessage;
 
 public class SendMail {
 	public void sendMail(String toEamil) {
-		final String user = "asdf@naver.com"; 					//�߽����� �̸��� ���̵� �Է�
-		final String password = ""; 							//�߽��� �̸����� �н����带 �Է�
+		final String user = "asdf@naver.com"; 					// 보내는사람 아이디
+		final String password = ""; 							//보내는사람 비밀번호
 		
 		Properties prop = new Properties();
-		prop.put("mail.smtp.host", "smtp.naver.com");			//�����ϰ�� smtp.gmail.com
+		prop.put("mail.smtp.host", "smtp.naver.com");			//구글 메일이면 smtp.gmail.com
 		prop.put("mail.smtp.port", 465);
 		prop.put("mail.smtp.auth", "true");
 		prop.put("mail.smtp.ssl.enable", "true");
@@ -28,25 +28,22 @@ public class SendMail {
                 return new PasswordAuthentication(user, password);
             }
         });
-
-		
-		
 		
 		try {
             MimeMessage message = new MimeMessage(session);
             message.setFrom(new InternetAddress(user));
 
-            //�����ڸ����ּ�
+            //메일 수신자
             message.addRecipient(Message.RecipientType.TO, new InternetAddress(toEamil));
 
             // Subject
-            message.setSubject("������ �Է��ϼ���"); //���� ������ �Է�
+            message.setSubject("보내기 제목입니다."); //제목 적어주는칸
 
             // Text
-            message.setText("������ �Է��ϼ���");    //���� ������ �Է�
+            message.setText("보내기 내용입니다.");    // 내용 적어주는칸
 
             // send the message
-            Transport.send(message); ////����
+            Transport.send(message); //전송
             System.out.println("message sent successfully...");
         } catch (AddressException e) {
             // TODO Auto-generated catch block
@@ -58,12 +55,12 @@ public class SendMail {
 	}
 	
 	
-	public void sendMail(String toEmail, String subject, String msg) {
-		final String user = "asdf@naver.com"; 					//�߽����� �̸��� ���̵� �Է�
-		final String password = ""; 							//�߽��� �̸����� �н����带 �Է�
+	public void sendMail(String toEmail, String subject, String msg) {	//받는사람  / 제목 / 내용 
+		final String user = "asdf@naver.com"; 					// 보내는사람 아이디
+		final String password = ""; 							// 보내는사람 비밀번호
 		
 		Properties prop = new Properties();
-		prop.put("mail.smtp.host", "smtp.naver.com");			//�����ϰ�� smtp.gmail.com
+		prop.put("mail.smtp.host", "smtp.naver.com");			//구글계정이면  smtp.gmail.com
 		prop.put("mail.smtp.port", 465);
 		prop.put("mail.smtp.auth", "true");
 		prop.put("mail.smtp.ssl.enable", "true");
@@ -79,7 +76,7 @@ public class SendMail {
             MimeMessage message = new MimeMessage(session);
             message.setFrom(new InternetAddress(user));
 
-            //�����ڸ����ּ�
+            //수신자 설정
             message.addRecipient(Message.RecipientType.TO, new InternetAddress(toEmail)); 
 
             // Subject
@@ -89,7 +86,7 @@ public class SendMail {
             message.setText(msg);    
 
             // send the message
-            Transport.send(message); ////����
+            Transport.send(message); //// 전송
             System.out.println("message sent successfully...");
         } catch (AddressException e) {
             // TODO Auto-generated catch block
