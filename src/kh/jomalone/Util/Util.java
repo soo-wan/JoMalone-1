a3fd4d5ba82ab6dbddea143259c2fbffa336f3d4
gh <gh@kh.com>
2019-11-29 12:03:36 +0900

    complet

---
+++ src/kh/jomalone/Util/Util.java
@@ -0,0 +1,13 @@
+package kh.jomalone.Util;
+
+public class Util {
+	private String ProtectXSS(String value) {
+		value = value.replaceAll("<", "& lt;").replaceAll(">", "& gt;");
+		value = value.replaceAll("\\(", "& #40;").replaceAll("\\)", "& #41;");
+		value = value.replaceAll("'", "& #39;");
+		value = value.replaceAll("eval\\((.*)\\)", "");
+		value = value.replaceAll("[\\\"\\\'][\\s]*javascript:(.*)[\\\"\\\']", "\"\"");
+		value = value.replaceAll("script", "");
+		return value;
+	}
+}
