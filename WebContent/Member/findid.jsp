ca3b0b5c70d276ed4f3335341e5f896fda2aad7e
kh <kh@kh.com>
2019-12-04 15:14:43 +0900

    adminpage css

--- WebContent/Member/findid.jsp
+++ WebContent/Member/findid.jsp
@@ -6,18 +6,27 @@
 <meta charset="UTF-8">
 <title>Insert title here</title>
 <script src="https://code.jquery.com/jquery-3.3.1.min.js"></script>
+<link rel="stylesheet" href="/JoMalone/Resource/css/find.css?log=1">
 </head>
 <body>
-
-이름을 입력해주세요
-<input type="text" id="name" name="name">
-핸드폰번호를 입력해주세요
-<input type="text" id="phone" name="phone">
-
-<input type="button" id="findid" value="확인">
-
+<jsp:include page="/Resource/key/top.jsp" flush="false"/>
+<div id="find" class="container">
+	<div id="find-title" class="row">
+		<h4>Find ID</h4>
+</div>
+<div id="findinputbox">
+이름을 입력해주세요<br>
+<input type="text" id="name" name="name"><br>
+'-'를 붙혀 핸드폰번호를 입력해주세요<br>
+<input type="text" id="phone" name="phone" placeholder="'-'를 붙혀 번호를 입력해주세요"><br>
+<br>
+<div id="findbtn">
+<input type="button"  id="findid" value="확인">
+</div>
+<br>
 <div id="checkid"></div>
-
+</div>
+</div>
 <script>
 $("#findid").on("click",function(){
 	$.ajax({
@@ -30,12 +39,16 @@ $("#findid").on("click",function(){
 		},
 		dataType:"json"
 	}).done(function(data){
-		console.log(data.id);
-		$("#checkid").html("고객님의 아이디는 "+data.id+"입니다");
+		if(data.id != null){
+		$("#checkid").html("고객님의 아이디는 "+data.id+"입니다");			
+		}else if(data.nullid != null){
+			alert("존재하지 않는 정보 입니다");
+		}
 		
 		
 	})
 })
 </script>
+<jsp:include page="/Resource/key/bottom.jsp" flush="false"/>
 </body>
 </html>
--- src/kh/jomalone/controller/findController.java
+++ src/kh/jomalone/controller/findController.java
@@ -28,9 +28,14 @@ public class findController extends HttpServlet {
 				String phone = request.getParameter("phone");
 				String id = dao.findid(name, phone);
 				
-				String subid = id.substring(0,id.length()-2)+"**";
-				System.out.println(subid);
-				response.getWriter().append("{ \"id\" : \"" + subid + "\"}");
+				if(id != null) {
+					String subid = id.substring(0,id.length()-2)+"**";
+					System.out.println(subid);
+					response.getWriter().append("{ \"id\" : \"" + subid + "\"}");	
+				}else if(id == null){
+					System.out.println("nu");
+					response.getWriter().append("{\"nullid\" : \"nullid\"}");
+				}
 				
 				
 			}else if(cmd.contentEquals("/Member/pw.find")) {
@@ -38,7 +43,7 @@ public class findController extends HttpServlet {
 	    		String toEmail = request.getParameter("email");
 	    		String id = request.getParameter("id");
 	    		
-	    		boolean result = dao.isEmailExist(toEmail);
+	    		boolean result = dao.isEmailExist(toEmail,id);
 	    		System.out.println(result);
 	    		
 	    		if(result) {
@@ -55,7 +60,8 @@ public class findController extends HttpServlet {
 	    			
 	    			SendMail send = new SendMail();
 	    			send.pwsendMail(toEmail, randomPw);
-	    			response.sendRedirect("login.jsp");
+	    			request.setAttribute("result", result);
+	    			request.getRequestDispatcher("findpw.jsp").forward(request, response);
 	    			
 	    		}else {
 	    			request.setAttribute("result", result);
