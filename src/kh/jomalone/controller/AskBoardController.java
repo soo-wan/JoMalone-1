7902a3e427f821312a11a1dc42e6a47d3da8fbc7
pj <pj@pj.com>
2019-12-30 14:34:29 +0900

    update 12_30

--- src/kh/jomalone/controller/AskBoardController.java
+++ src/kh/jomalone/controller/AskBoardController.java
@@ -7,12 +7,6 @@ import java.util.ArrayList;
 import java.util.Enumeration;
 import java.util.List;
 
-import javax.servlet.ServletException;
-import javax.servlet.annotation.WebServlet;
-import javax.servlet.http.HttpServlet;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-
 import com.google.gson.Gson;
 import com.google.gson.JsonArray;
 import com.google.gson.JsonObject;
